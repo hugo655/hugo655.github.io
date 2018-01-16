b4ecd5a4899b85514e89333d6d845d8ef7346ecc
hugo655 <gayosohugo@gmail.com>
2018-01-16 19:08:47 -0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -16,14 +16,14 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.textContent = 'Mozilla is cool, ' + myName;
+  myHeading.textContent = 'Oi, ' + myName;
 }
 
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  myHeading.textContent = 'Oi, ' + storedName;
 }
 
 

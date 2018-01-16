883a6b28bc718c4d1843e38369840e8ff767ceac
hugo655 <gayosohugo@gmail.com>
2018-01-16 17:13:16 -0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function(){
+	var mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/image-1.svg'){
+		myImage.setAttribute('src','images/image-2.svg')
+	} else{
+		myImage.setAttribute('src','images/image-1.svg')
+	}
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+
+myButton.onclick = function() {
+  setUserName();
+}
